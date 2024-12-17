332e595724789cabdbaa96b0fc602d7ceaaa79ee
Mark Shcerbakov <markshcerbakov@gmail.com>
2024-12-17 21:47:22 +0300

    Commit 5

--- TMSHomeWorkLesson1/Program.cs
+++ TMSHomeWorkLesson1/Program.cs
@@ -2,7 +2,7 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Homework after lesson 1 in TMS school.");
 
-Console.WriteLine("Введите ваше ФИО:");
+Console.WriteLine("Введите ваше ФИО:"); // Commit 5
 string name = Console.ReadLine();
 Console.WriteLine("Введите вашу дату рождения (дд.мм.гггг):");
 string birthdate = Console.ReadLine(); // Commit 4
